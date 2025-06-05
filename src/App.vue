6f3ce3587d1f2b5e27c6cc657b4018679ce8b9e1
Ryan <ryanshaunsmith@gmail.com>
2025-06-05 14:25:48 +0100

    create a task app using vue.js off the back of the syntax crash course

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import TaskForm from "./components/TaskForm.vue";
+import TaskList from "./components/TaskList.vue";
+import type { Task, TaskFilter } from "./types";
+import FilterButton from "./components/FilterButton.vue";
+
+const message = ref("Tasks App");
+const tasks = ref<Task[]>([]);
+const filter = ref<TaskFilter>("all");
+
+const totalDone = computed(() =>
+  tasks.value.reduce((total, task) => (task.done ? total + 1 : total), 0)
+);
+
+const filteredTasks = computed(() => {
+  switch (filter.value) {
+    case "done":
+      return tasks.value.filter((task) => task.done);
+
+    case "todo":
+      return tasks.value.filter((task) => !task.done);
+    default:
+      return tasks.value;
+  }
+});
+
+function addTask(newTask: string) {
+  tasks.value.push({
+    id: crypto.randomUUID(),
+    title: newTask,
+    done: false,
+  });
+}
+
+function toggleDone(id: string) {
+  const task = tasks.value.find((task) => task.id === id);
+  if (task) {
+    task.done = !task?.done;
+  }
+}
+
+function removeTask(id: string) {
+  const indexToRemove = tasks.value.findIndex((task) => task.id === id);
+  if (indexToRemove !== -1) {
+    tasks.value.splice(indexToRemove, 1);
+  }
+}
+
+function setFilter(value: TaskFilter) {
+  filter.value = value;
+}
+</script>
+
+<template>
+  <main>
+    <h1>{{ message }}</h1>
+    <TaskForm @add-task="addTask" />
+    <h3 v-if="!tasks.length">Add a task to get started</h3>
+    <h3 v-else>{{ totalDone }} / {{ tasks.length }} tasks completed</h3>
+    <div v-if="tasks.length" class="button-container">
+      <FilterButton :currentFilter="filter" filter="all" @set-filter="setFilter" />
+      <FilterButton :currentFilter="filter" filter="todo" @set-filter="setFilter" />
+      <FilterButton :currentFilter="filter" filter="done" @set-filter="setFilter" />
+    </div>
+    <TaskList :tasks="filteredTasks" @toggle-done="toggleDone" @remove-task="removeTask" />
+  </main>
+</template>
+
+<style>
+main {
+  max-width: 800px;
+  margin: 1rem auto;
+  padding: 0 1rem;
+}
+
+.button-container {
+  display: flex;
+  justify-content: end;
+  gap: 0.5rem;
+}
+</style>
